0044608a53c58b3ca29fe6756ceadd401c9ba567
yumirak <yumirashadow@gmail.com>
2026-02-18 09:55:12 +0700

    game: conform flag status against Proto91

--- code/qcommon/q_shared.h
+++ code/qcommon/q_shared.h
@@ -1419,9 +1419,9 @@ typedef enum {
 typedef enum _flag_status {
 	FLAG_ATBASE = 0,
 	FLAG_TAKEN,			// CTF
+	FLAG_DROPPED,
 	FLAG_TAKEN_RED,		// One Flag CTF
 	FLAG_TAKEN_BLUE,	// One Flag CTF
-	FLAG_DROPPED
 } flagStatus_t;
 
 
